1490b7c171c9d2a914bfe244735ec963666157a4
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-04-10 13:01:14 +0100

    ♻️ Changed reduce for sum

--- src/narcissistic_number_kata/narcissistic_number.py
+++ src/narcissistic_number_kata/narcissistic_number.py
@@ -1,8 +1,4 @@
-import functools
-
-
 def is_narcissistic(given_number: int) -> bool:
     digits = [int(n) for n in str(given_number)]
     calculated_digits = list(map(lambda d: pow(d, len(digits)), digits))
-    sum_of_digits = functools.reduce(lambda a, b: a + b, calculated_digits)
-    return sum_of_digits == given_number
+    return sum(calculated_digits) == given_number
