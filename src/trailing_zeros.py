fa288194485d82f7b8b0ef787b8f21ec07e7ac83
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-06-25 23:27:14 +0100

    ♻️ Improved magic numbers

--- src/trailing_zeros.py
+++ src/trailing_zeros.py
@@ -5,7 +5,9 @@ def trailing_zeros(given_number: int) -> int:
     divider = 5
     divider_factorial = divider
     result = 0
-    while floor(given_number / divider_factorial) > 0:
-        result += floor(given_number / divider_factorial)
+    calculation = floor(given_number / divider_factorial)
+    calculation_is_positive = calculation > 0
+    while calculation_is_positive:
+        result += calculation
         divider_factorial *= divider
     return result
