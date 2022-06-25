e1c2c51e419b2a0952532dd8a8f363d46bf428d4
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-06-25 23:54:31 +0100

    ♻️ Simplify ad_valid_parentheses

--- src/valid_parentheses.py
+++ src/valid_parentheses.py
@@ -1,19 +1,10 @@
-def had_valid_parentheses(input: str) -> bool:
-    valid_parentheses_values = ['(', ')']
-    parentheses = [p for p in input if p in valid_parentheses_values]
-    return _had_the_same_pairs(parentheses)
-
-
-def _had_the_same_pairs(parentheses) -> bool:
-    open_parentheses = '('
-    close_parentheses = ')'
-    if not parentheses:
-        return True
-
-    if parentheses[0] is close_parentheses or not close_parentheses in parentheses[0:]:
-        return False
-
-    if parentheses[0] is open_parentheses and close_parentheses in parentheses[0:]:
-        parentheses.remove(parentheses[0])
-        parentheses.pop(parentheses[0:].index(close_parentheses))
-    return _had_the_same_pairs(parentheses)
+def had_valid_parentheses(line: str) -> bool:
+    counter = 0
+    for character in line:
+        if character == '(':
+            counter += 1
+        if character == ')':
+            counter -= 1
+        if counter < 0:
+            return False
+    return True if counter == 0 else False
