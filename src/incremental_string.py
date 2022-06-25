58dece38c27ad22824f911825f8e1afca62e1e72
Wolfremium13 <wolfremiuminformatica@gmail.com>
2022-06-26 00:02:56 +0100

    ♻️ Simplify increment_the_end_number_by_one

--- src/incremental_string.py
+++ src/incremental_string.py
@@ -1,19 +1,9 @@
 import re
 
 
-def increment_the_end_number_by_one(input: str) -> str:
-    matched_digits = re.match('.*?([0-9]+)$', input)
-    if not matched_digits:
-        return input + "1"
-
-    end_digits = [d for d in matched_digits.group(1)]
-    result = int(''.join(end_digits)) + 1
-    result_digits = [d for d in str(result)]
-    replaced_digits = end_digits[:len(result_digits) * -1] + result_digits
-
-    input_characters = [c for c in input]
-    difference = len(replaced_digits) - len(end_digits)
-    replaced_input = input_characters[:(len(
-        replaced_digits) - difference) * -1] + replaced_digits
-
-    return ''.join(replaced_input)
+def increment_the_end_number_by_one(line: str) -> str:
+    head = line.rstrip('0123456789')
+    tail = line[len(head):]
+    if tail == "":
+        return line + "1"
+    return head + str(int(tail) + 1).zfill(len(tail))
